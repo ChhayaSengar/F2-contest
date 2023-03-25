555815e31ad4f78a9f8f051e72b351dd5714c2f8
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-03-25 23:37:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const biodata = {
+    repositories: [
+        {
+            title: "getcursor/cursor",
+            description: "An editor made for programming with AI",
+            url: "https://github.com/getcursor/cursor",
+            stars: "2737",
+            forks: "",
+            language: "Typescript"
+        },
+        {
+            title: "openai/chatgpt-retrieval-plugin",
+            description: "",
+            url: "https://github.com/openai/chatgpt-retrieval-plugin",
+            stars: "6442",
+            forks: "",
+            language: "Python" 
+        }
+    ]
+
+};
+
